a726a016b686baec97fd1b6cada7556c1c6b17b2
Levi Carlos <olevicarlos.dev@gmail.com>
2025-04-30 16:24:23 -0300

    resolvendo exercicio-15.js(o pior de todos)

--- lista-exercicios-1/exercicio-15.js
+++ lista-exercicios-1/exercicio-15.js
@@ -1,1 +1,13 @@
 const prompt = require("prompt-sync")();
+
+const numero = Number(prompt("Digite o número: "));
+let anterior = 0;
+let atual = 1;
+let proximoNumero = null;
+
+for (let i = 0; i < numero; i++) {
+  proximoNumero = anterior;
+  anterior = atual;
+  atual += proximoNumero;
+}
+console.log(anterior);
