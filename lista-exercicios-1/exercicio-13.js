b8558a6ef3c984656b1f12f824b4160ebc311e2c
Levi Carlos <olevicarlos.dev@gmail.com>
2025-04-30 16:09:04 -0300

    resolvendo exercicio-13.js

--- lista-exercicios-1/exercicio-13.js
+++ lista-exercicios-1/exercicio-13.js
@@ -1,1 +1,22 @@
 const prompt = require("prompt-sync")();
+
+let opcao = null;
+let totalNumeros = 0;
+let somaNumeros = 0;
+let media = 0;
+
+while (opcao != 0) {
+  opcao = Number(prompt("Digite um numero: "));
+
+  if (isNaN(opcao)) {
+    console.log("número inválido");
+    continue;
+  }
+  if (opcao != 0) {
+    somaNumeros += opcao;
+    totalNumeros++;
+  } else {
+    media = somaNumeros / totalNumeros;
+  }
+}
+console.log("A média dos números é", media.toFixed(3));
