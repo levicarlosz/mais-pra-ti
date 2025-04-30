f05f4d9ea7b9cae7274f8bbe854159add1473a5e
Levi Carlos <olevicarlos.dev@gmail.com>
2025-04-30 15:44:20 -0300

    resolvendo exercicio-11.js

--- lista-exercicios-1/exercicio-11.js
+++ lista-exercicios-1/exercicio-11.js
@@ -1,1 +1,8 @@
 const prompt = require("prompt-sync")();
+
+let somaTotal = 0;
+for (let i = 0; i < 5; i++) {
+  let numeroDigitado = Number(prompt("Digite o numero: "));
+  somaTotal += numeroDigitado;
+}
+console.log("A soma total é " + somaTotal);
