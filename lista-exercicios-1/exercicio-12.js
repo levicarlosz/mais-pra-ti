698b0a068d254243131aec88b6201e7c7fc62f6c
Levi Carlos <olevicarlos.dev@gmail.com>
2025-04-30 15:49:12 -0300

    resolvendo exercicio-12.js

--- lista-exercicios-1/exercicio-12.js
+++ lista-exercicios-1/exercicio-12.js
@@ -1,1 +1,9 @@
 const prompt = require("prompt-sync")();
+
+const numeroDigitado = Number(
+  prompt("Digite o número que deseja obter a Tabuada: ")
+);
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} X ${numeroDigitado} = ${numeroDigitado * i}`);
+}
