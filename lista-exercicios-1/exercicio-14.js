0e7652af2a5f34d4de5a319c592ea8410909fa06
Levi Carlos <olevicarlos.dev@gmail.com>
2025-04-30 16:15:07 -0300

    resolvendo o exercicio-14.js

--- lista-exercicios-1/exercicio-14.js
+++ lista-exercicios-1/exercicio-14.js
@@ -1,1 +1,9 @@
 const prompt = require("prompt-sync")();
+
+const numero = Number(prompt("Digite o número: "))
+let resultado = 1
+
+for(let i = 2; i <= numero; i++){
+    resultado *= i
+}
+console.log(resultado)
